1ba6d9dfa24e6f7a4974fe6ea0b4bc1f25871734
Jachi97 <62693399+Jachi97@users.noreply.github.com>
2020-12-16 00:18:17 +0100

    Segunda Parte de registro de usuarios
    - Ahora el frame de registro muestra correctamente el tipo de cuenta que se va a crear, junto a los parámetros requeridos para la creación de cada tipo de cuenta.

--- ServiLearn/Registro.cs
+++ ServiLearn/Registro.cs
@@ -20,12 +20,56 @@ namespace ServiLearn
         {
             InitializeComponent();
             tipo = t;
+
+            label4.Hide();
+            label5.Hide();
+            label6.Hide();
+            label7.Hide();
+            label8.Hide();
+            label9.Hide();
+            label10.Hide();
+            textBox2.Hide();
+            textBox3.Hide();
+            textBox4.Hide();
+
+            if (t == 0) {
+                label4.Show();
+            }
+
+            if (t == 3) {
+                label5.Show();
+                label10.Show();
+                textBox2.Show();
+                label9.Show();
+                textBox3.Show();
+                label8.Show();
+                textBox4.Show();
+            }
+
+            if (t == 2) {
+                label6.Show();
+                label10.Show();
+                textBox2.Show();
+                label9.Show();
+                textBox3.Show();
+            }
+
+            if (t == 1) {
+                label7.Show();
+                label10.Show();
+                textBox2.Show();
+            }
         }
 
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             this.Close();
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
 
         }
     }
--- ServiLearn/Registro.designer.cs
+++ ServiLearn/Registro.designer.cs
@@ -31,11 +31,20 @@ namespace ServiLearn
             this.label1 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.button2 = new System.Windows.Forms.Button();
-            this.tbBuscadorEvento = new System.Windows.Forms.TextBox();
+            this.textBox0 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -62,42 +71,42 @@ namespace ServiLearn
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
-            this.button2.Location = new System.Drawing.Point(281, 261);
+            this.button2.Location = new System.Drawing.Point(364, 491);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(320, 160);
+            this.button2.Size = new System.Drawing.Size(171, 49);
             this.button2.TabIndex = 8;
             this.button2.Text = "Aceptar";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // tbBuscadorEvento
+            // textBox0
             // 
-            this.tbBuscadorEvento.Location = new System.Drawing.Point(479, 125);
-            this.tbBuscadorEvento.Name = "tbBuscadorEvento";
-            this.tbBuscadorEvento.Size = new System.Drawing.Size(93, 20);
-            this.tbBuscadorEvento.TabIndex = 9;
+            this.textBox0.Location = new System.Drawing.Point(479, 147);
+            this.textBox0.Name = "textBox0";
+            this.textBox0.Size = new System.Drawing.Size(134, 20);
+            this.textBox0.TabIndex = 9;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(479, 185);
+            this.textBox1.Location = new System.Drawing.Point(479, 206);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(93, 20);
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
             this.textBox1.TabIndex = 10;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(338, 120);
+            this.label2.Location = new System.Drawing.Point(250, 142);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 25);
+            this.label2.Size = new System.Drawing.Size(197, 25);
             this.label2.TabIndex = 11;
-            this.label2.Text = "Usuario";
+            this.label2.Text = "Nombre de Usuario";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(301, 180);
+            this.label3.Location = new System.Drawing.Point(290, 201);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(123, 25);
             this.label3.TabIndex = 12;
@@ -113,16 +122,107 @@ namespace ServiLearn
             this.label4.TabIndex = 13;
             this.label4.Text = "de Invitado";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(561, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(233, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "de ONG / Adm. Pública";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(561, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "de Tutor";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(561, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 25);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "de Alumno";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(479, 266);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(479, 328);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(134, 20);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(479, 390);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(134, 20);
+            this.textBox4.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(296, 385);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(102, 25);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Dirección";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(302, 323);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 25);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Teléfono";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(257, 261);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(190, 25);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Correo Electrónico";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
             // Registro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(920, 606);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.tbBuscadorEvento);
+            this.Controls.Add(this.textBox0);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label1);
@@ -138,12 +238,23 @@ namespace ServiLearn
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox tbBuscadorEvento;
+        private System.Windows.Forms.TextBox textBox0;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
+
+
 }
 
 
