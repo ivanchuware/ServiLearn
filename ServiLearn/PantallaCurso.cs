eb6d827b6f0e004644833cd03ab91fc58edc9038
AaronHDN <aaronmape@hotmail.com>
2021-01-12 12:07:01 +0100

    Requisito 5

--- ServiLearn/PantallaCurso.cs
+++ ServiLearn/PantallaCurso.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BDLibrary;
 
 namespace ServiLearn
 {
@@ -15,6 +16,9 @@ namespace ServiLearn
         private Curso seleccionado;
         private Cuenta user;
         private int tipo;
+        private int selectedUsuario = -1;
+        List<object[]> tuplasUsuario;
+        List<Tuple<int, int>> indiceIdUsuario;
 
         public PantallaCurso(Cuenta u, int t, Curso cur)
         {
@@ -30,11 +34,58 @@ namespace ServiLearn
 
             bModCurso.Visible = false;
             bEliCurso.Visible = false;
+            bExpulsar.Visible = false;
+
             if (seleccionado.IdOwner == user.id || tipo == 4)
 
             {
                 bModCurso.Visible = true;
                 bEliCurso.Visible = true;
+                bExpulsar.Visible = true;
+            }
+            consultarUsuarios();
+            actualizarUsuarios();
+        }
+
+        private void consultarUsuarios()
+        {
+            try
+            {
+                MySQLDB miBD = new MySQLDB();
+                tuplasUsuario = miBD.Select("SELECT Cuenta.id_Cuenta, nombre FROM Cuenta JOIN Cuenta_Curso ON Cuenta.id_Cuenta = Cuenta_Curso.id_Cuenta AND Cuenta_Curso.id_Curso =" +seleccionado.Id+ ";");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void actualizarUsuarios()
+        {
+            lbUsuariosInscritos.Items.Clear();
+            string filter = tbBuscarUsuario.Text;
+            indiceIdUsuario = new List<Tuple<int, int>>();
+            int index = 0;
+            foreach (object[] tupla in tuplasUsuario)
+            {
+                try
+                {
+
+
+                    string nombre = (string)tupla[1];
+                    if (nombre.Contains(filter))
+                    {
+
+                        lbUsuariosInscritos.Items.Add(nombre);
+                        indiceIdUsuario.Add(new Tuple<int, int>(index, (int)tupla[0]));
+                        index++;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
             }
         }
 
@@ -83,6 +134,74 @@ namespace ServiLearn
         private void tvAdic_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void bExpulsar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult dialogResult = MessageBox.Show("¿Esta seguro de que quiere expulsar al usuario seleccionado?", "Advertencia", MessageBoxButtons.YesNo);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    user = new Cuenta(selectedUsuario);
+                    tipo = obtenerTipo(user);
+                    MySQLDB miBD = new MySQLDB();
+                    miBD.Delete("DELETE FROM Cuenta_Curso where id_Cuenta = " + user.id + ";");
+                    selectedUsuario = -1;
+                    MessageBox.Show("Usuario expulsado con exito");
+                    actualizarUsuarios();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR: " + ex.Message);
+            }
+        }
+
+        private int obtenerTipo(Cuenta c)
+        {
+            if (Invitado.esInvitado(user.id))
+            {
+                tipo = 0;
+            }
+            else if (Alumno.esAlumno(user.id))
+            {
+                tipo = 1;
+            }
+            else if (Docente.esDocente(user.id))
+            {
+                tipo = 2;
+            }
+            else if (ONG.esOng(user.id))
+            {
+                tipo = 3;
+            }
+            else
+            {
+                tipo = 4;
+            }
+
+            return tipo;
+        }
+
+        private void tbBuscarUsuario_TextChanged(object sender, EventArgs e)
+        {
+            actualizarUsuarios();
+        }
+
+        private void lbUsuariosInscritos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedUsuario = -1;
+            foreach (Tuple<int, int> tupla in indiceIdUsuario)
+            {
+
+                if (lbUsuariosInscritos.SelectedIndex == tupla.Item1)
+                {
+                    selectedUsuario = tupla.Item2;
+                }
+            }
         }
     }
 }
