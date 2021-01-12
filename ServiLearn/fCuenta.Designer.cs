70a56038dc67a92677530c783afca30ba5375cdf
ivanchuware <igl.igl70@gmail.com>
2021-01-12 11:10:58 +0100

    FCuenta de nuevo

--- ServiLearn/fCuenta.Designer.cs
+++ ServiLearn/fCuenta.Designer.cs
@@ -33,6 +33,13 @@ namespace ServiLearn
             this.lTipo = new System.Windows.Forms.Label();
             this.bCambiarNombre = new System.Windows.Forms.Button();
             this.bCambiarContraseña = new System.Windows.Forms.Button();
+            this.bEliminarCuenta = new System.Windows.Forms.Button();
+            this.lDireccion = new System.Windows.Forms.Label();
+            this.bDirección = new System.Windows.Forms.Button();
+            this.lEmail = new System.Windows.Forms.Label();
+            this.bEmail = new System.Windows.Forms.Button();
+            this.lTlfn = new System.Windows.Forms.Label();
+            this.bTlfn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lNombre
@@ -47,7 +54,7 @@ namespace ServiLearn
             // lTipo
             // 
             this.lTipo.AutoSize = true;
-            this.lTipo.Location = new System.Drawing.Point(33, 80);
+            this.lTipo.Location = new System.Drawing.Point(33, 75);
             this.lTipo.Name = "lTipo";
             this.lTipo.Size = new System.Drawing.Size(76, 13);
             this.lTipo.TabIndex = 1;
@@ -55,7 +62,7 @@ namespace ServiLearn
             // 
             // bCambiarNombre
             // 
-            this.bCambiarNombre.Location = new System.Drawing.Point(36, 169);
+            this.bCambiarNombre.Location = new System.Drawing.Point(36, 267);
             this.bCambiarNombre.Name = "bCambiarNombre";
             this.bCambiarNombre.Size = new System.Drawing.Size(118, 23);
             this.bCambiarNombre.TabIndex = 2;
@@ -65,7 +72,7 @@ namespace ServiLearn
             // 
             // bCambiarContraseña
             // 
-            this.bCambiarContraseña.Location = new System.Drawing.Point(36, 209);
+            this.bCambiarContraseña.Location = new System.Drawing.Point(36, 296);
             this.bCambiarContraseña.Name = "bCambiarContraseña";
             this.bCambiarContraseña.Size = new System.Drawing.Size(118, 23);
             this.bCambiarContraseña.TabIndex = 3;
@@ -73,11 +80,82 @@ namespace ServiLearn
             this.bCambiarContraseña.UseVisualStyleBackColor = true;
             this.bCambiarContraseña.Click += new System.EventHandler(this.bCambiarContraseña_Click);
             // 
+            // bEliminarCuenta
+            // 
+            this.bEliminarCuenta.Location = new System.Drawing.Point(36, 344);
+            this.bEliminarCuenta.Name = "bEliminarCuenta";
+            this.bEliminarCuenta.Size = new System.Drawing.Size(118, 23);
+            this.bEliminarCuenta.TabIndex = 4;
+            this.bEliminarCuenta.Text = "Eliminar Cuenta";
+            this.bEliminarCuenta.UseVisualStyleBackColor = true;
+            this.bEliminarCuenta.Click += new System.EventHandler(this.bEliminarCuenta_Click);
+            // 
+            // lDireccion
+            // 
+            this.lDireccion.AutoSize = true;
+            this.lDireccion.Location = new System.Drawing.Point(33, 141);
+            this.lDireccion.Name = "lDireccion";
+            this.lDireccion.Size = new System.Drawing.Size(52, 13);
+            this.lDireccion.TabIndex = 5;
+            this.lDireccion.Text = "Dirección";
+            // 
+            // bDirección
+            // 
+            this.bDirección.Location = new System.Drawing.Point(144, 136);
+            this.bDirección.Name = "bDirección";
+            this.bDirección.Size = new System.Drawing.Size(107, 23);
+            this.bDirección.TabIndex = 6;
+            this.bDirección.Text = "Cambiar Direccion";
+            this.bDirección.UseVisualStyleBackColor = true;
+            // 
+            // lEmail
+            // 
+            this.lEmail.AutoSize = true;
+            this.lEmail.Location = new System.Drawing.Point(33, 112);
+            this.lEmail.Name = "lEmail";
+            this.lEmail.Size = new System.Drawing.Size(31, 13);
+            this.lEmail.TabIndex = 7;
+            this.lEmail.Text = "email";
+            // 
+            // bEmail
+            // 
+            this.bEmail.Location = new System.Drawing.Point(144, 107);
+            this.bEmail.Name = "bEmail";
+            this.bEmail.Size = new System.Drawing.Size(107, 23);
+            this.bEmail.TabIndex = 8;
+            this.bEmail.Text = "Cambiar Email";
+            this.bEmail.UseVisualStyleBackColor = true;
+            // 
+            // lTlfn
+            // 
+            this.lTlfn.AutoSize = true;
+            this.lTlfn.Location = new System.Drawing.Point(33, 171);
+            this.lTlfn.Name = "lTlfn";
+            this.lTlfn.Size = new System.Drawing.Size(49, 13);
+            this.lTlfn.TabIndex = 9;
+            this.lTlfn.Text = "Telefono";
+            // 
+            // bTlfn
+            // 
+            this.bTlfn.Location = new System.Drawing.Point(144, 165);
+            this.bTlfn.Name = "bTlfn";
+            this.bTlfn.Size = new System.Drawing.Size(107, 23);
+            this.bTlfn.TabIndex = 10;
+            this.bTlfn.Text = "Cambiar Telefono";
+            this.bTlfn.UseVisualStyleBackColor = true;
+            // 
             // fCuenta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(454, 450);
+            this.Controls.Add(this.bTlfn);
+            this.Controls.Add(this.lTlfn);
+            this.Controls.Add(this.bEmail);
+            this.Controls.Add(this.lEmail);
+            this.Controls.Add(this.bDirección);
+            this.Controls.Add(this.lDireccion);
+            this.Controls.Add(this.bEliminarCuenta);
             this.Controls.Add(this.bCambiarContraseña);
             this.Controls.Add(this.bCambiarNombre);
             this.Controls.Add(this.lTipo);
@@ -95,5 +173,12 @@ namespace ServiLearn
         private System.Windows.Forms.Label lTipo;
         private System.Windows.Forms.Button bCambiarNombre;
         private System.Windows.Forms.Button bCambiarContraseña;
+        private System.Windows.Forms.Button bEliminarCuenta;
+        private System.Windows.Forms.Label lDireccion;
+        private System.Windows.Forms.Button bDirección;
+        private System.Windows.Forms.Label lEmail;
+        private System.Windows.Forms.Button bEmail;
+        private System.Windows.Forms.Label lTlfn;
+        private System.Windows.Forms.Button bTlfn;
     }
 }
--- ServiLearn/fCuenta.cs
+++ ServiLearn/fCuenta.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BDLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,9 @@ namespace ServiLearn
     {
         private Cuenta user;
         private int tipo;
+        private string email;
+        private int telefono;
+        private string direccion;
         public fCuenta(Cuenta u, int t)
         {
             InitializeComponent();
@@ -24,6 +28,24 @@ namespace ServiLearn
         }
         private void actualizarCuenta()
         {
+            if (tipo > 0)
+            {
+                try
+                {
+                    lEmail.Visible = true;
+                    bEmail.Visible = true;
+                } catch (Exception ex)
+                {
+
+                }
+                if (tipo == 2 || tipo == 3)
+                {
+                    lTlfn.Visible = true;
+                    bTlfn.Visible = true;
+
+                }
+                
+            }
             user = new Cuenta(user.id);
             lNombre.Text = "Nombre de usuario: " + user.nombre;
             String tp;
@@ -61,6 +83,61 @@ namespace ServiLearn
             fCambiarClave ventana = new fCambiarClave(user);
             ventana.ShowDialog();
             actualizarCuenta();
+        }
+
+        private void bEliminarCuenta_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("¿Estas seguro de que quieres eliminar esta cuenta?", "", MessageBoxButtons.YesNo);
+            try
+            {
+                if (dialogResult == DialogResult.Yes)
+                {
+                    String tipoidCuenta;
+                    String tipodelete;
+                    switch (tipo)
+                    {
+                        case 0:
+                            tipoidCuenta = "id_Invitado";
+                            tipodelete = "Invitado";
+                            break;
+                        case 1:
+                            tipoidCuenta = "id_Alumno";
+                            tipodelete = "Alumno";
+                            break;
+                        case 2:
+                            tipoidCuenta = "id_Docente";
+                            tipodelete = "Docente";
+                            break;
+                        case 3:
+                            tipoidCuenta = "id_Ong";
+                            tipodelete = "Ong";
+                            break;
+                        default:
+                            tipoidCuenta = "id_Admin";
+                            tipodelete = "Admin";
+                            break;
+                    }
+                    MySQLDB miBD = new MySQLDB();
+                    miBD.Delete("DELETE FROM " + tipodelete + " where " + tipoidCuenta + " = " + user.Id + ";");
+                    miBD.Delete("DELETE FROM Cuenta_Curso where id_Cuenta = " + user.Id + ";");
+                    
+                    miBD.Delete("DELETE FROM Cuenta_Evento where id_Cuenta = " + user.Id + ";");
+                    
+                    miBD.Delete("DELETE FROM Cuenta where id_Cuenta = " + user.Id + ";");
+                    MessageBox.Show("Cuenta borrada con exito");
+                    this.Close();
+                    Principal p = (Principal)Application.OpenForms["Principal"];
+                    p.Close();
+
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
